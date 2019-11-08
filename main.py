8e6d8f666f89dddffb315fd6482351902472588e
Solidras <40754850+Solidras@users.noreply.github.com>
2019-11-08 14:09:45 +0900

    Added characters and quote search
    Characters are now loaded from a file.

--- main.py
+++ main.py
@@ -13,15 +13,22 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+#Must be https://discordapp.com/api/webhooks/your_id with no token after the id
 WEBHOOK_URL = os.getenv('WEBHOOK_URL')
-WEBHOOK_ID = os.getenv('WEBHOOK_ID')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+WEBHOOK_ID = WEBHOOK_URL.split('/')[-1]
 
 bot = commands.Bot(command_prefix='!')
-
-characters = {'perceval': 'https://cdn.discordapp.com/attachments/550631040826343431/642161266286264320/perceval.jpg',
-			  'loth':'https://i.imgur.com/0KSpe7H.jpg'}
-				#'loth', 'leodagan', 'merlin', 'roi_burgonde', 'karadoc', 'arthur', 'kadoc', 'seli', 'yvain', 'bohort']
+			  
+async def load_characters():
+	c = {}
+	f = open('characters.txt', 'r')
+	for line in f.readlines():
+		character,url = line.split(',')
+		c[character] = url
+	f.close()
+	return c
 
 @bot.event
 async def on_ready():
@@ -56,7 +63,8 @@ async def change_webhook_channel(id):
 					  
 	response = request.urlopen(req)
 
-async def random_quotes(character):
+async def random_quotes(character, words):
+	characters = await load_characters()
 	character = character.lower()
 	
 	if not character in characters:
@@ -65,15 +73,35 @@ async def random_quotes(character):
 	f = open('quotes/' + character + '.txt', 'r')
 	quotes = f.read().split('#')
 	f.close()
+	
+	#If a word is in more than 1 quote, it doesn't always be the same quote.
+	random.shuffle(quotes)
+	
+	notFindText = ["Ouais, c'est pas faux.", "Non mais je connais pas ce mot là."]
+	
+	response = ''
+	if not words:
+		response = random.choice(quotes)
+	else:
+		for q in quotes:
+			if words.lower() in q.lower():
+				response = q
+	if not response:
+		response = random.choice(notFindText)
 
-	response = random.choice(quotes)
 	return response
 
-async def send_webhook_message(ctx, character):
+async def send_webhook_message(ctx, arg):
+	characters = await load_characters()
+	character = arg[0]
+	words = ''
+	if len(arg) > 1:
+		words = ' '.join(arg[1:])
+		
 	webhook = await get_webhook(ctx)
 	if webhook != None:
 		await change_webhook_channel(ctx.channel.id)
-		content = await random_quotes(character)
+		content = await random_quotes(character, words)
 		await webhook.send(content=content, username=character.capitalize(), avatar_url=characters[character])
 	
 
@@ -83,12 +111,20 @@ async def on_message(message):
 		return
 
 	if message.content.startswith('!'):
+		characters = await load_characters()
 		content = message.content[1:]
 		arg = content.split()
 		if arg[0] in characters:
 			await message.delete()
-			await send_webhook_message(message, arg[0])
+			await send_webhook_message(message, arg)
 			
 	await bot.process_commands(message)
+	
+@bot.event
+async def on_command_error(ctx, error):
+	characters = await load_characters()
+	if ctx.message.content.split()[0][1:] in characters:
+		return
+	raise error
 
 bot.run(BOT_TOKEN)
